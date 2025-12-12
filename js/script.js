3d2d71e9e7fd1ad1e5c0597f4c4893ef2006a959
JuliaMuk <46424816+JuliaMuk@users.noreply.github.com>
2025-12-12 10:18:10 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const balls = document.querySelectorAll('.ball');
+
+balls.forEach((ball)=>{
+    ball.addEventListener("click", (e)=> {
+        e.stopPropagation();
+        ball.classList.add("zoom")
+        setTimeout(()=>{
+            ball.classList.add("show")
+        }, 3000)
+    })    
+})
+
+const closeBtns = document.querySelectorAll(".btn-close")
+closeBtns.forEach((btn)=>{
+    btn.addEventListener("click", (e)=> {
+        e.stopPropagation();
+        btn.closest(".ball").classList.remove("show");
+        btn.closest(".ball").classList.remove("zoom");
+    })    
+})
+
+
+const container = document.querySelector(".container")
+container.addEventListener("click", ()=>{
+    balls.forEach((ball)=>{
+        ball.classList.remove("show");
+        ball.classList.remove("zoom");
+    })    
+})
