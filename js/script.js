48f6abff3c04c3b83c919406dd985b756c3a5078
JuliaMuk <46424816+JuliaMuk@users.noreply.github.com>
2025-12-26 11:47:11 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
-const balls = document.querySelectorAll('.ball');
+const balls = document.querySelectorAll(".ball");
+let timer
+balls.forEach((ball) => {
+  ball.addEventListener("click", (e) => {
+    e.stopPropagation();
+    ball.classList.add("zoom");
+    timer = setTimeout(() => {
+      ball.classList.add("show");
+    }, 2000);
+  });
+});
 
-balls.forEach((ball)=>{
-    ball.addEventListener("click", (e)=> {
-        e.stopPropagation();
-        ball.classList.add("zoom")
-        setTimeout(()=>{
-            ball.classList.add("show")
-        }, 2000)
-    })    
-})
+const closeBtns = document.querySelectorAll(".btn-close");
+closeBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    btn.closest(".ball").classList.remove("show");
+    btn.closest(".ball").classList.remove("zoom");
+  });
+});
 
-const closeBtns = document.querySelectorAll(".btn-close")
-closeBtns.forEach((btn)=>{
-    btn.addEventListener("click", (e)=> {
-        e.stopPropagation();
-        btn.closest(".ball").classList.remove("show");
-        btn.closest(".ball").classList.remove("zoom");
-    })    
-})
-
-
-const container = document.querySelector(".container")
-container.addEventListener("click", ()=>{
-    balls.forEach((ball)=>{
-        ball.classList.remove("show");
-        ball.classList.remove("zoom");
-    })    
-})
+const container = document.querySelector(".container");
+container.addEventListener("click", () => {
+    clearTimeout(timer);
+    balls.forEach((ball) => {
+    ball.classList.remove("show");
+    ball.classList.remove("zoom");
+  });
+});
